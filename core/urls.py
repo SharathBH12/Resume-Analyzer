2fe480eb88f00198810f23d1a6eaab0a8049065c
SharathBH <sharathtavanandi25@gmail.com>
2026-05-25 22:26:56 +0530

    Updated urls

--- core/urls.py
+++ core/urls.py
@@ -1,20 +1,25 @@
 from django.contrib import admin
 from django.urls import path
+
 from analyzer import views
 
+
 urlpatterns = [
-    
+
+    # Admin Panel
     path('admin/', admin.site.urls),
 
+    # Home Page
     path('', views.home, name='home'),
 
+    # Authentication
     path('register/', views.register, name='register'),
-
     path('login/', views.login_page, name='login'),
-
-    path('dashboard/', views.dashboard, name='dashboard'),
-    
-    path('upload/', views.upload_resume, name='upload'),
-
     path('logout/', views.logout_page, name='logout'),
+
+    # Dashboard
+    path('dashboard/', views.dashboard, name='dashboard'),
+
+    # Resume Analyzer
+    path('upload/', views.upload_resume, name='upload'),
 ]
